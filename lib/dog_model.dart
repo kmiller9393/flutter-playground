64c8b5e838c62b7c88a979549e1ee2058335dcc0
kmiller9393 <kurt@fluidtruck.com>
2020-01-05 16:56:25 -0700

    First commit, create Dog Model Class

---
+++ lib/dog_model.dart
@@ -0,0 +1,10 @@
+class Dog {
+  final String name;
+  final String location;
+  final String description;
+  String imageUrl;
+
+  int rating = 10;
+
+  Dog(this.name, this.location, this.description);
+}
